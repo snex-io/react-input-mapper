5d277f13d418704d1fd3742ddd085981a5524174
Pontus Persson <pontus.alexander@gmail.com>
2017-08-11 11:23:31 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# SNEX React Input Mapper
+
+A widget that shows a controller on screen and lets the user map visible buttons to keyboard keys. The component comes functional styles included but requires cosmetic styling for your particular project.
+
+## Usage
+
+* Install.
+```bash
+yarn add @snex/react-input-mapper
+```
+
+* Require component.
+```js
+import InputMapper from '@snex/react-input-mapper';
+```
+
+* Setup and render.
+```jsx
+<InputMapper
+  svgURL='/graphics/nes.svg'
+  onEmit={(key, state) => console.log(`Button ${key} is in state ${state}`)}
+/>
+```
+
+## Example style
+```css
+.App .snex-input-mapper {
+    font-size: 2vh;
+    margin: 0 auto;
+    width: 30vw;
+}
+
+.App .snex-input-mapper .key-map {
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: .2em;
+    padding: 1em;
+    text-align: left;
+}
+```
---
+++ src/index.jsx
@@ -0,0 +1,253 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+const MESSAGE_AUTOHIDE = 3 * 1000;
+const ESC = 27;
+
+const DEFAULT = {
+  65: "LEFT",
+  68: "RIGHT",
+  87: "UP",
+  83: "DOWN",
+  80: "A",
+  79: "B",
+  81: "SELECT",
+  69: "START",
+};
+
+function saveMap(key, map) {
+  window.localStorage.setItem(key, JSON.stringify(map));
+}
+
+function loadMap(key) {
+  const map = localStorage.getItem(key);
+  if (map) {
+    try {
+      return JSON.parse(map);
+    } catch (e) {
+      console.error("Map restore failed", e);
+      return DEFAULT;
+    }
+  }
+  return DEFAULT;
+}
+
+const HUMAN_CODES = {
+  37: 11104,
+  38: 11105,
+  39: 11106,
+  40: 11107,
+};
+
+function keyCodeToHuman(keyCode) {
+  return HUMAN_CODES[keyCode]
+    ? String.fromCharCode(HUMAN_CODES[keyCode])
+    : String.fromCharCode(keyCode) || keyCode;
+}
+
+class InputMapper extends Component {
+  static propTypes = {
+    onEmit: PropTypes.func.isRequired,
+    svgURL: PropTypes.string.isRequired,
+    localStorageKey: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    localStorageKey: "snex-react-input-mapper",
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: null,
+      map: loadMap(props.localStorage),
+      showMessage: false,
+      showMap: false,
+      waitingForInput: null,
+    };
+
+    this.keyOrder = [];
+
+    this.onInput = this.onInput.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.onInput);
+    window.addEventListener("keyup", this.onInput);
+
+    this.svgNode.addEventListener("load", () => {
+      this.initialize(this.svgNode.contentDocument);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.onInput);
+    window.removeEventListener("keyup", this.onInput);
+  }
+
+  initialize(svgNode) {
+    this.keyOrder = [];
+    const nodes = svgNode.querySelectorAll("[id]");
+    for (let i = 0, node; (node = nodes[i]); ++i) {
+      const id = node.getAttribute("id");
+      if (id.startsWith("snex-button-")) {
+        const name = id.replace("snex-button-", "");
+        this.keyOrder.push(name);
+        node.addEventListener("click", () => this.enableRemap(name));
+      }
+    }
+  }
+
+  emitMessage(message, hold = false) {
+    this.setState({
+      message,
+      showMessage: true,
+    });
+
+    if (!hold) {
+      clearTimeout(this.messageTimer);
+      this.messageTimer = setTimeout(() => {
+        this.cancelMessage();
+      }, MESSAGE_AUTOHIDE);
+    }
+  }
+
+  cancelMessage() {
+    this.setState({
+      showMessage: false,
+    });
+  }
+
+  enableRemap(keyName) {
+    const text = `Press any key to remap "${keyName}".`;
+    this.emitMessage(text, true);
+    this.setState({
+      showMap: false,
+      waitingForInput: keyName,
+    });
+    window.focus();
+  }
+
+  cancelRemap() {
+    this.setState({
+      waitingForInput: null,
+    });
+  }
+
+  handleInputWhileRemapping({ keyCode }) {
+    if (keyCode === ESC) {
+      this.cancelMessage();
+      this.cancelRemap();
+      return;
+    }
+
+    const keyName = this.state.waitingForInput;
+    const map = Object.assign({}, this.state.map, { [keyCode]: keyName });
+
+    const text = `Mapped "${keyName}" to ${keyCodeToHuman(keyCode)}.`;
+    this.cancelRemap();
+    this.emitMessage(text, false);
+
+    this.setState({ map });
+    saveMap(this.props.localStorageKey, map);
+  }
+
+  onInput(event) {
+    if (this.state.waitingForInput) {
+      return this.handleInputWhileRemapping(event);
+    }
+
+    const { keyCode } = event;
+    const { map } = this.state;
+    if (map[keyCode]) this.props.onEmit(map[keyCode], event.type);
+  }
+
+  render() {
+    const { map } = this.state;
+    const reverseMap = Object.keys(map).reduce((reverseMap, keyCode) => {
+      reverseMap[map[keyCode]] = keyCode;
+      return reverseMap;
+    }, {});
+
+    return (
+      <div className="snex-input-mapper" style={{ position: "relative" }}>
+        <div
+          className="key-map"
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: [
+              "translateX(-50%)",
+              "translateY(-50%)",
+              `rotateX(${this.state.showMap ? 0 : 90}deg)`,
+            ].join(" "),
+            transition: "transform 0.5s ease",
+            position: "absolute",
+            zIndex: 2,
+          }}
+        >
+          <table>
+            {this.keyOrder.map(keyName => {
+              return (
+                <tr>
+                  <th>
+                    {keyName}
+                  </th>
+                  <td>
+                    {keyCodeToHuman(reverseMap[keyName])}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+
+          <a
+            className="hide-key-map"
+            style={{ cursor: "pointer" }}
+            onClick={() => this.setState({ showMap: false })}
+          >
+            Hide Key Map
+          </a>
+        </div>
+
+        <div className="controller" style={{ position: "relative" }}>
+          <object
+            data={this.props.svgURL}
+            type="image/svg+xml"
+            style={{
+              width: "100%",
+            }}
+            ref={node => (this.svgNode = node)}
+          />
+        </div>
+
+        <div style={{ display: "flex", position: "absolute", width: "100%" }}>
+          <div
+            className="message"
+            style={{
+              margin: "auto",
+              textAlign: "center",
+              top: "100%",
+            }}
+          >
+            {this.state.showMessage
+              ? this.state.message
+              : !this.state.showMap
+                ? <a
+                    className="show-key-map"
+                    style={{ cursor: "pointer" }}
+                    onClick={() =>
+                      this.setState({ showMap: !this.state.showMap })}
+                  >
+                    Show Key Map
+                  </a>
+                : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputMapper;
